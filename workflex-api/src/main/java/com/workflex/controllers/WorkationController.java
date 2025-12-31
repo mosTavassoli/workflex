8671f4463e56e9f1d087fb21f25b28ac18f22160
mostafa tavassoli <m.tavassoli89@gmail.com>
2025-12-31 05:43:52 +0100

    add update endpoint

--- workflex-api/src/main/java/com/workflex/controllers/WorkationController.java
+++ workflex-api/src/main/java/com/workflex/controllers/WorkationController.java
@@ -6,6 +6,7 @@ import com.workflex.services.WorkationService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
@@ -27,13 +28,27 @@ public class WorkationController {
 
 
     @GetMapping("/{id}")
-    public WorkationDto getById(@PathVariable Long id){
+    public WorkationDto getById(@PathVariable Long id) {
         return workationService.getWorkationById(id);
     }
 
     @PostMapping("")
-    @ResponseStatus(HttpStatus.CREATED)
-    public WorkationDto createWorkation(@Valid @RequestBody WorkationDto body){
-        return workationService.createWorkation(body);
+    public ResponseEntity<WorkationDto> createWorkation(@Valid @RequestBody WorkationDto body) {
+        WorkationDto created = workationService.createWorkation(body);
+        return ResponseEntity.status(HttpStatus.CREATED).body(created);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<WorkationDto> updateWorkation(
+            @PathVariable Long id,
+            @Valid @RequestBody WorkationDto body) {
+        WorkationDto updated = workationService.updateWorkation(id, body);
+        return ResponseEntity.ok(updated);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteWorkation(@PathVariable Long id){
+        workationService.deleteWorkation(id);
+        return ResponseEntity.noContent().build();
     }
 }
--- workflex-api/src/main/java/com/workflex/repositories/WorkationRepository.java
+++ workflex-api/src/main/java/com/workflex/repositories/WorkationRepository.java
@@ -5,19 +5,25 @@ import com.workflex.domain.models.Workation;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 
 public interface WorkationRepository extends JpaRepository<Workation, Long> {
 
     @Query("""
-                 SELECT w FROM Workation w
-                 WHERE (:employee IS NULL\s
-                        OR LOWER(w.employee) LIKE LOWER(CONCAT('%', :employee, '%')))
-                   AND (:originCountry IS NULL\s
-                        OR w.originCountry = :originCountry)
-            \s""")
+            SELECT w FROM Workation w
+            WHERE w.deletedAt IS NULL
+               AND  (:employee IS NULL OR LOWER(w.employee) LIKE LOWER(CONCAT('%', :employee, '%')))
+               AND (:originCountry IS NULL OR w.originCountry = :originCountry)
+            """)
     List<Workation> search(
             @Param("employee") String employee,
             @Param("originCountry") String originCountry
     );
+
+
+    Optional<Workation> findByIdAndDeletedAtIsNull(Long id);
+
+    List<Workation> findAllByDeletedAtIsNull();
 }
